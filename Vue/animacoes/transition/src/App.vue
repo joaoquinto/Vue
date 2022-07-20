2ce696421e2eeb2bce6962f7f3f261e3a609f4cf
Joaoquinto <joaovictoroliveirapereira6522@gmail.com>
2022-07-19 22:29:13 -0300

    animacoes

--- Vue/animacoes/transition/src/App.vue
+++ Vue/animacoes/transition/src/App.vue
@@ -2,10 +2,12 @@
   <div id="app">
     <transition-vue />
     <modal-vue />
+    <componentes-vue />
   </div>
 </template>
 
 <script>
+import ComponentesVue from "./components/Componentes.vue";
 import ModalVue from "./components/Modal.vue";
 import TransitionVue from "./components/Transition.vue";
 
@@ -14,6 +16,7 @@ export default {
   components: {
     TransitionVue,
     ModalVue,
+    ComponentesVue,
   },
 };
 </script>
---
+++ Vue/animacoes/transition/src/components/Componentes.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <transition mode="out-in">
+      <p v-if="estoque > 0" key="estoque">
+        Itens em estoque {{ estoque === 0 ? null : estoque }}
+      </p>
+      <p v-else key="esgotado">Produtos fora de estoque</p>
+    </transition >
+    <button @click="estoque++">Adicionar Estoque</button>
+    <button @click="estoque = 0">Resetar estoque</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "componente-vue",
+  data() {
+    return {
+      estoque: 1,
+    };
+  },
+};
+</script>
+
+<style>
+.v-enter-active,
+.v-leave-active {
+  transition: opacity 0.3s;
+}
+
+.v-enter,
+.v-leave-to {
+  opacity: 0;
+}
+</style>
